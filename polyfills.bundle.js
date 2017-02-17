c1304103dc2315b170d27b3fd99fb32de4eedd76
okamanda <olivier.kamanda@pif.gov>
2017-02-17 16:04:33 -0500

    Updates

--- polyfills.bundle.js
+++ polyfills.bundle.js
@@ -130,7 +130,7 @@ var ac_polyfills =
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/site/presidential-innovation-fellows/code-gov-web/";
 
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
